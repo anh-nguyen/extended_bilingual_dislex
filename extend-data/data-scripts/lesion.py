4c46230e48a33bec075b89e8de47d4f5b68cc593
Anh Nguyen <anguyen@haverford.edu>
2016-03-27 11:09:44 -0400

    lesion script done

--- extend-data/data-scripts/lesion.py
+++ extend-data/data-scripts/lesion.py
@@ -21,44 +21,76 @@ def lesion_assocs (patient_file, lesion_file, map_sizes, num_reps, noise_amount,
 		if "network-weights" in line:
 			break
 
-	assocs_start =  line_num + (map_sizes["l1"]**2)*num_reps["l1"] + (map_sizes["l2"]**2)*num_reps["l2"] + (map_sizes["sem"]**2)*num_reps["sem"] + 1
+	l1_lines = (map_sizes["l1"]**2)*num_reps["l1lex"] + (map_sizes["l1"]**2)*num_reps["l1phonol"] + (map_sizes["l1"]**2)*num_reps["l1phonetic"]
+	l2_lines = (map_sizes["l2"]**2)*num_reps["l2lex"] + (map_sizes["l2"]**2)*num_reps["l2phonol"] + (map_sizes["l2"]**2)*num_reps["l2phonetic"] 
+	assocs_start =  line_num + l1_lines + l2_lines + (map_sizes["sem"]**2)*num_reps["sem"] + 1
 
 	# order in the file: sl1 assocs, sl2 assocs, l1l2 assocs
 
 	sl1lex_start = assocs_start
-	sl2lex_start = _________________
-	l1lexphonol_start = ________
-	l2lexphonol_start = ____________
-	l2lexphonol_inclusive_end = 
+	sl2lex_start = sl1lex_start + (map_sizes["l1"]**2)*(map_sizes["sem"]**2)*2 
+	l1l2lex_start = sl2lex_start + (map_sizes["l2"]**2)*(map_sizes["sem"]**2)*2 
+	l1lexphonol_start = l1l2lex_start + (map_sizes["l1"]**2)*(map_sizes["l2"]**2)*2 
+	l2lexphonol_start = l1lexphonol_start + (map_sizes["l1"]**2)*(map_sizes["l1"]**2)*2
+	l2lexphonol_exclusive_end = l2lexphonol_start + + (map_sizes["l2"]**2)*(map_sizes["l2"]**2)*2
 
 	# iterate until we reach assoc weights
 	for line in patient:	# starting from network-weights
 		line_num += 1
 		lesioned.write(line)
-		if line_num == assocs_start - 1:
+		if line_num == sl1lex_start - 1:
 			break
 
 	# add noise to sl1 weights
 	for line in patient:
 		line_num += 1
 		orig = float(line.strip())
-		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["sl1"])
+		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["sl1lex"])
 		with_noise = with_noise if with_noise > 0 else 0
 		lesioned.write("{:.6f}\n".format(with_noise))
-		if line_num == sl2_start - 1:
+		if line_num == sl2lex_start - 1:
 			break
 
 	# add noise to sl2 weights
 	for line in patient:
 		line_num += 1
 		orig = float(line.strip())
-		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["sl2"])
+		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["sl2lex"])
 		with_noise = with_noise if with_noise > 0 else 0
 		lesioned.write("{:.6f}\n".format(with_noise))
-		if line_num == l1l2_start - 1:
+		if line_num == l1l2lex_start - 1:
 			break
 
-	# etc.
+	# iterate again until we reach the lex phonol weights
+	for line in patient:
+		line_num += 1
+		lesioned.write(line)
+		if line_num == l1lexphonol_start - 1:
+			break
+
+	# add noise to L1 lex phonol weights
+	for line in patient:
+		line_num += 1
+		orig = float(line.strip())
+		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["l1lexphonol"])
+		with_noise = with_noise if with_noise > 0 else 0
+		lesioned.write("{:.6f}\n".format(with_noise))
+		if line_num == l2lexphonol_start - 1:
+			break
+
+	# add noise to L2 lex phonol weights
+	for line in patient:
+		line_num += 1
+		orig = float(line.strip())
+		with_noise = orig + gauss(GAUSSIAN_MEAN, noise_amount["l2lexphonol"])
+		with_noise = with_noise if with_noise > 0 else 0
+		lesioned.write("{:.6f}\n".format(with_noise))
+		if line_num == l2lexphonol_exclusive_end - 1:
+			break
+
+	# continue writing the rest
+	for line in patient:
+		lesioned.write(line)
 
 
 if __name__ == "__main__":
@@ -70,7 +102,7 @@ if __name__ == "__main__":
 	UTBA20_noise_amount = {'sl1lex': 0, 'sl2lex': 0, 'l1lexphonol' : 0, 'l2lexphonol' : 0}
 	UTBA21_noise_amount = {'sl1lex': 0, 'sl2lex': 0, 'l1lexphonol' : 0, 'l2lexphonol' : 0}
 
-	UTBA18_overwrite = False
+	UTBA18_overwrite = True
 	BUBA01_overwrite = False
 	UTBA20_overwrite = False
 	UTBA21_overwrite = False
